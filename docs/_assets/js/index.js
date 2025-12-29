426507d5e1ff46012c594783f6c3faea4c2f4985
zhangyongjie <zhangyongjie@caas>
2025-12-29 18:05:55 +0800

    [fix]修改广告的样式

--- docs/_assets/js/index.js
+++ docs/_assets/js/index.js
@@ -3,20 +3,27 @@ document.addEventListener('DOMContentLoaded', () => {
         mutations.forEach(m => {
             m.addedNodes.forEach(node => {
                 if (node.nodeType === Node.ELEMENT_NODE) {
-                    console.log('新增元素:', node);
+                    if (node.id == 'readthedocs-ea-text-nostyle-nodoctool') {
+                        // 插入 广告标记
+                        var htmlString = `<div class="icon">📢</div>`;
+                        node.insertAdjacentHTML('afterbegin', htmlString);
+                    } else {
+                        // 隐藏
+                        node.hidden = true;
+                    }
                 }
             });
 
         });
         //
-        document.getElementById('readthedocs-ea-text-nostyle-nodoctool').addEventListener('click', function() {
+        document.getElementById('readthedocs-ea-text-nostyle-nodoctool').addEventListener('click', function () {
             this.classList.toggle('expanded');
         });
     });
 
     // 确保 body 存在
     if (document.body) {
-        observer.observe(document.body, { childList: true, subtree: true });
+        observer.observe(document.body, {childList: true, subtree: true});
     } else {
         console.error('document.body 不存在！');
     }
